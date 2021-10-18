b3fff1710d4bd556e5876cbd23fa1852868ff49e
Budu0101 <60315319+Budu0101@users.noreply.github.com>
2021-10-18 19:49:42 +0300

    Revert "Added Comments, Functions of OOP and checking if the ping was success…"

--- ping_website.py
+++ ping_website.py
@@ -1,17 +1,12 @@
-#importing the necessary libraries
 import subprocess
 
-def ping(host):
-    #using the built-in ping command on Windows
-    ping = subprocess.Popen(
+host = "www.google.com"
+
+ping = subprocess.Popen(
     ["ping", "-n", "1", host],
     stdout = subprocess.PIPE,
     stderr = subprocess.PIPE
 )
-    #checking if "reply" exists in the response
-    out, error = ping.communicate()
-    if "Reply" in str(out):
-        return "Successful"
-    else:
-        return "Unsuccessful"
-print(ping("www.google.com"))
+
+out, error = ping.communicate()
+print(out)
